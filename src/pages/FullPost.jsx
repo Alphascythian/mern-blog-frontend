872fd2dc62a0a92e127fcd7ab4f4da0978cd0829
Alphascythian <abyssmal@mail.ru>
2023-05-21 18:51:50 +0100

    commit

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -8,7 +8,7 @@ export const FullPost = () => {
   return (
     <>
       <Post
-        id={1}
+        _id={1}
         title="Roast the code #1 | Rock Paper Scissors"
         imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
         user={{
